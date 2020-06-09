b76d1e4655748b1c791b8108af92d3b61481745e
Jasmine Huang <tinjasminee@gmail.com>
2020-06-09 11:36:16 +0800

    supplier verification

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :products
+  namespace :suppliers do
+    resources :verifications, only: [:edit, :update]
+  end
 end
