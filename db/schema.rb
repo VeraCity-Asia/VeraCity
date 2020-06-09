9eed2756752bedecbcbc7c115941795a284df9fa
applesnort <jmangin@gmail.com>
2020-06-09 15:25:06 +0800

    adding kind to licenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_071917) do
+ActiveRecord::Schema.define(version: 2020_06_09_072322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_071917) do
     t.bigint "supplier_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "kind"
     t.index ["supplier_id"], name: "index_licenses_on_supplier_id"
   end
 
