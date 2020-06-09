5d7e527e2dfadef4d339e86e00c662fdf96143be
applesnort <jmangin@gmail.com>
2020-06-09 15:22:52 +0800

    additional columns to suppliers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_024322) do
+ActiveRecord::Schema.define(version: 2020_06_09_071917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,12 @@ ActiveRecord::Schema.define(version: 2020_06_09_024322) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "industry"
+    t.string "delivery_terms"
+    t.string "payment_terms"
+    t.string "nearest_port"
+    t.boolean "whitelisted"
+    t.date "established"
     t.index ["user_id"], name: "index_suppliers_on_user_id"
   end
 
