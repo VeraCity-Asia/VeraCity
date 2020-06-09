b9e59bd93c0fa920f348a1d2e7f4924d6abdbde3
Jasmine Huang <tinjasminee@gmail.com>
2020-06-09 16:07:26 +0800

    finish all routes

---
+++ app/controllers/suppliers/products_controller.rb
@@ -0,0 +1,17 @@
+class Suppliers::ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :edit]
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
