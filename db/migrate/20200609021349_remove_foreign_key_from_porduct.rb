d4597d962809e6a6b95bebeb419bc335ae4ed33a
Christine Rhee <rheec2016@gmail.com>
2020-06-09 15:35:49 +0900

    banner added

--- db/migrate/20200609021349_remove_foreign_key_from_porduct.rb
+++ db/migrate/20200609021349_remove_foreign_key_from_porduct.rb
@@ -1,4 +1,4 @@
-class RemoveForeignKeyFromPorduct < ActiveRecord::Migration[6.0]
+class RemoveForeignKeyFromProduct < ActiveRecord::Migration[6.0]
   def change
     remove_foreign_key :products, column: :certification_id
     remove_reference(:products, :certifications, index: true, foreign_key: true)
