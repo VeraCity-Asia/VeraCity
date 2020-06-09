035f369b2b7ed3e4bcce1ee4f70a196f69688d08
Jasmine Huang <tinjasminee@gmail.com>
2020-06-09 13:56:01 +0800

    add purchaser namespace

---
+++ app/controllers/purchasers/offers_controller.rb
@@ -0,0 +1,17 @@
+class Purchasers::OffersController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:show, :index]
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
     resources :licenses, only: [:new, :create, :destroy]
     resources :offers, only: [:index, :show, :update]
   end
+  namespace :purchasers do
+    resources :offers, only: [:new, :show, :index, :create, :delete]
+  end
 end
