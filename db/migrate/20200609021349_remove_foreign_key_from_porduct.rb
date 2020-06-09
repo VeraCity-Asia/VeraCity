2da11f64c85e54d2cd014090d9b5662de7a2f832
Joel Mangin <1295334+applesnort@users.noreply.github.com>
2020-06-09 17:14:46 +0800

    Delete 20200609021349_remove_foreign_key_from_porduct.rb
    We are preserving the misspelling so as not to introduce entropy.

--- db/migrate/20200609021349_remove_foreign_key_from_porduct.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeyFromProduct < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :products, column: :certification_id
-    remove_reference(:products, :certifications, index: true, foreign_key: true)
-  end
-end
