761d494865ec63ddc77e20b4b3a4391dc408c18f
Jasmine Huang <tinjasminee@gmail.com>
2020-06-09 21:59:52 +0800

    half way of license, supplier issue here

--- app/controllers/suppliers/licenses_controller.rb
+++ app/controllers/suppliers/licenses_controller.rb
@@ -1,10 +1,27 @@
 class Suppliers::LicensesController < ApplicationController
+
   def new
+    @license = License.new
   end
 
   def create
+    @license = License.new(license_params)
+    raise
+    if @license.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @license = License.find(params[:id])
+    @license.destroy
+  end
+
+  private
+
+  def license_params
+    params.require(:license).permit(:authority, :number)
   end
 end
