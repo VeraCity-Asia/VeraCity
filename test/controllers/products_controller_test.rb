2e30539dba4a2beae58bd1b850b5293e3e8bf892
Jasmine Huang <tinjasminee@gmail.com>
2020-06-09 11:19:40 +0800

    create products controller and routes

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get products_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get products_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get products_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get products_edit_url
+    assert_response :success
+  end
+
+end
