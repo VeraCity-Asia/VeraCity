325543a21b4b9e1505755d47ee8b056d0a0a59a9
Jasmine Huang <tinjasminee@gmail.com>
2020-06-10 20:37:32 +0800

    add general filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,11 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
     @products = Product.all
+    @query = params[:query]
+
+    if @query.present?
+      @products = @products.search_by_name_certification(@query)
+    end
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,19 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_and_belongs_to_many :offers
   has_and_belongs_to_many :certifications
+  has_many :messages
 
   validates :name, :price, :category, :production_quantity, :minimum_order_quantity, presence: true
 
+  #pgsearch
+  #for general search
+  include PgSearch::Model
+  pg_search_scope :search_by_name_certification,
+    against: [:name, :category],
+  associated_against: {
+    certifications: [:category, :number]
+  },
+  using: {
+    tsearch: { prefix: true}
+  }
 end
