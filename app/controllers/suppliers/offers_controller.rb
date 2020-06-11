e4f80745a2921bd473179a76be5e16e733e28a4d
Jasmine Huang <tinjasminee@gmail.com>
2020-06-11 16:56:31 +0800

    update button

--- app/controllers/suppliers/offers_controller.rb
+++ app/controllers/suppliers/offers_controller.rb
@@ -1,5 +1,5 @@
 class Suppliers::OffersController < ApplicationController
-  before_action :find_offer, only: [:show,:update, :approved, :rejected, :generateoffer]
+  before_action :find_offer, only: [:show,:update,:generateoffer]
   def index
     @offers = policy_scope([:suppliers, Offer])
   end
@@ -8,30 +8,25 @@ class Suppliers::OffersController < ApplicationController
     # policy_class: app/policies/suppliers/offer_policy#show
     authorize([:suppliers, @offer])
   end
-  
-  
-  
-  def approved
-    @offer.approved
-    @offer.update
+
+
+
+  def update
+    @offer.update(offer_params)
     # policy_class: app/policies/suppliers/offer_policy#show
     authorize([:suppliers, @offer])
-    redirect_to generateoffer_suppliers_offer_path(@offer)
+    redirect_to suppliers_offer_path(@offer)
   end
 
-  def rejected
-    @offer.rejected
-    @offer.update
-  end
-
-  def generateoffer
-    #get offer id from approve form
-  end
 
 
   private
 
   def find_offer
     @offer = Offer.find(params[:id])
+  end
+
+  def offer_params
+    params.permit(:approved)
   end
 end
