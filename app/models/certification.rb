5c3d0b52bf34e295fd54025236c325dccf36ef0f
Jasmine Huang <tinjasminee@gmail.com>
2020-06-11 12:02:13 +0800

    add certification validation

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,4 +1,6 @@
 class Certification < ApplicationRecord
+  #validation
+  CATEGORY = ["CE", "FDA", "ISO"]
   has_and_belongs_to_many :products
   validates :number, :validity, :category, :listing_number, :code, :authority, presence: true
   #selection
