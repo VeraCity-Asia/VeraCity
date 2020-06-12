ccae3670462df39d574323ba97c1496d9a7fe196
Jasmine Huang <tinjasminee@gmail.com>
2020-06-12 13:44:46 +0800

    add offer logic

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -8,5 +8,9 @@ class Offer < ApplicationRecord
   validates :amount, :destination, :price, :payment, presence: true
   validates :payment, inclusion: { in: PAYMENT }
 
+  def self.add(user, product)
+    return if user.user_type == "supplier"
 
+    offer = self.find_by(approved: nil, user: user, supplier: product.supplier)
+  end
 end
