7763c52279edab6efb7563c2d2c1d359e2cfce3d
Jasmine Huang <tinjasminee@gmail.com>
2020-06-17 21:21:07 +0800

    end date issue

--- app/models/license.rb
+++ app/models/license.rb
@@ -2,4 +2,12 @@ class License < ApplicationRecord
   belongs_to :supplier
   validates :authority, :number,:start_date, :expired_date, presence: true
   has_many_attached :photos
+
+  def end_after_start
+    return if expired_date.blank? || start_date.blank?
+
+    if expired_date< start_date
+      errors.add(:expired_date, "must be after the start date")
+    end
+  end
 end
