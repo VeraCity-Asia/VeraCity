dd494f5a66a26215ca1acafd58c478c10a74b108
Jasmine Huang <tinjasminee@gmail.com>
2020-06-17 22:40:02 +0800

    add status bar for edit form

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,5 +1,6 @@
 class License < ApplicationRecord
   belongs_to :supplier
+  validate :end_after_start
   validates :authority, :number,:start_date, :expired_date, presence: true
   has_many_attached :photos
 
