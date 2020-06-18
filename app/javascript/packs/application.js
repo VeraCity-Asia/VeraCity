3c0662b1621ebb71777fba835840489afa1a3437
Christine Rhee <rheec2016@gmail.com>
2020-06-18 14:47:40 +0900

    unused js commented out

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,12 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { productsFilter } from './products_filter';
-import { swiperNoSwiping } from './products_swiper';
+// import { productsFilter } from './products_filter';
+// import { swiperNoSwiping } from './products_swiper';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  swiperNoSwiping();
-  productsFilter();
+  // swiperNoSwiping();
+  // productsFilter();
 });
